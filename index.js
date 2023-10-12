04b6cd153d676d0493b112ca3d42d51fabd531cd
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-10-12 12:07:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    // Iterate through the array with the second number in the pair
+    for (let j = i + 1; j < array.length; j++) {
+      // Check if the pair of numbers adds up to the target
+      if (array[i] + array[j] === target) {
+        return true; // Found a pair with the target sum
+      }
+    }
+  }
+  // If no such pair is found, return false
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  The space complexity is O(1) 
+  The time complexity is O(n^2),nested loops result in quadratic time complexity. 
 */
 
 /* 
   Add your pseudocode here
+    For each number in the array:
+        For each subsequent number in the array:
+            If the current number and the subsequent number add up to the target:
+                Return True
+    Return False
 */
 
 /*
